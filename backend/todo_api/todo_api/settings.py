d6d15f2cf9f612481ff278ce8065649bd03f373f
Vasyl Pyrig <pyrigok@i.ua>
2018-08-31 16:07:10 +0300

    rest_framework and serialisers.py file was added

--- backend/todo_api/todo_api/settings.py
+++ backend/todo_api/todo_api/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'todos',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
--- backend/todo_api/todos/views.py
+++ backend/todo_api/todos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Todo
+
+from .serializers import TodoSerializer
+
+# two views will list all our todos 
+# and provide a detail view of a single todo object
+
+class ListTodo(generics.ListCreateAPIView):
+	queryset = Todo.objects.all()
+	serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Todo.objects.all()
+	serializer_class = TodoSerializer
